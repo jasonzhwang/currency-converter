0fb968038fbc2de556457d2067c25f667ac036c9
Jason Wang <0212jason@gmail.com>
2025-11-18 20:35:54 +1100

    chore: update husky v10, jest config, and lint-staged

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jest-environment-jsdom",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.svg$": "<rootDir>/__mocks__/svg.js",
+  },
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/**/*.stories.{js,jsx,ts,tsx}",
+    "!src/**/__tests__/**",
+    "!src/pages/_app.tsx",
+    "!src/pages/api/**",
+  ],
+  testMatch: [
+    "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
+    "<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}",
+  ],
+};
+
+module.exports = createJestConfig(customJestConfig);
